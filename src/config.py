ce3264f4421a6e07a5dcef943f1d744aa3608b6c
shiva-hadi1366 <shivahadi4@gmail.com>
2026-05-23 10:16:11 +0200

    Add files via upload

--- src/config.py
+++ src/config.py
@@ -1,1 +1,17 @@
+MODEL_PATH = "models/fraud_model.pkl"
+SCALER_PATH = "models/scaler.pkl"
+
+TRAIN_DATA_PATH = "data/train.csv"
+TEST_DATA_PATH = "data/test.csv"
+
+RANDOM_STATE = 42
+TEST_SIZE = 0.2
+
+FEATURES = [
+    "amount",
+    "transaction_type",
+    "country",
+    "device_type",
+    "time_since_last_tx"
+]
 
--- src/utils.py
+++ src/utils.py
@@ -1,1 +1,14 @@
+import pickle
+import pandas as pd
+
+def load_model(path):
+    with open(path, "rb") as f:
+        return pickle.load(f)
+
+def save_model(model, path):
+    with open(path, "wb") as f:
+        pickle.dump(model, f)
+
+def json_to_df(json_data):
+    return pd.DataFrame([json_data])
 
