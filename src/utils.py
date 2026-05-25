bd669d709cc224dde2ad7418c2bf36ef8b1e95df
shiva-hadi1366 <shivahadi4@gmail.com>
2026-05-25 13:43:59 +0200

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,6 +1,11 @@
 import pickle
 import pandas as pd
 
+# Imports needed for loading the model
+from sklearn.pipeline import Pipeline
+from sklearn.preprocessing import StandardScaler
+from sklearn.linear_model import LogisticRegression
+
 def load_model(path):
     with open(path, "rb") as f:
         return pickle.load(f)
@@ -11,4 +16,3 @@ def save_model(model, path):
 
 def json_to_df(json_data):
     return pd.DataFrame([json_data])
-
