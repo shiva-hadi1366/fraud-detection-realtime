b3cd99e4885aae8405d2913956d82641ff2832ec
shiva-hadi1366 <shivahadi4@gmail.com>
2026-05-25 13:46:37 +0200

    Update utils.py

--- src/utils.py
+++ src/utils.py
@@ -1,18 +1,18 @@
-import pickle
+import joblib
 import pandas as pd
 
-# Imports needed for loading the model
-from sklearn.pipeline import Pipeline
-from sklearn.preprocessing import StandardScaler
-from sklearn.linear_model import LogisticRegression
+# === Imports required for loading the model ===
+from imblearn.pipeline import Pipeline as ImbPipeline
+from sklearn.compose import ColumnTransformer
+from sklearn.preprocessing import OneHotEncoder
+from xgboost import XGBClassifier
+from imblearn.over_sampling import SMOTE
 
 def load_model(path):
-    with open(path, "rb") as f:
-        return pickle.load(f)
+    return joblib.load(path)
 
 def save_model(model, path):
-    with open(path, "wb") as f:
-        pickle.dump(model, f)
+    joblib.dump(model, path)
 
 def json_to_df(json_data):
     return pd.DataFrame([json_data])
